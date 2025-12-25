cac7ecdb995ad835b4b734c466546bfed8ac86d3
Ana Marcela <ana.marcela@hipcom.com.br>
2025-12-25 12:47:24 -0300

    Adicionado suporte para detecção de tipo de tela e métodos de responsividade

--- lib/core/responsividade.dart
+++ lib/core/responsividade.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 enum TipoTela { mobile, tablet, desktop }
 
 class ClotoResponsividade {
@@ -7,4 +9,17 @@ class ClotoResponsividade {
     return TipoTela.mobile;
   }
 
+  static double tamanhoDaTela(BuildContext context) {
+    final largura = MediaQuery.of(context).size.width;
+    return largura;
+  }
+
+  static bool mobile(BuildContext contexto) => MediaQuery.of(contexto).size.width < 700;
+
+  static bool tablet(BuildContext contexto) {
+    final largura = MediaQuery.of(contexto).size.width;
+    return largura >= 700 && largura < 1000;
+  }
+
+  static bool desktop(BuildContext contexto) => MediaQuery.of(contexto).size.width >= 1000;
 }
